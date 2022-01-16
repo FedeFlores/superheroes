19c8e00d588ed852e7d5e8e575e35efb8ac6018d
FedeFlores <48038093+FedeFlores@users.noreply.github.com>
2022-01-16 02:06:08 -0300

    controller tests

--- src/main/java/com/fedeflores/superheroes/service/SuperheroesService.java
+++ src/main/java/com/fedeflores/superheroes/service/SuperheroesService.java
@@ -1,4 +1,20 @@
 package com.fedeflores.superheroes.service;
 
+import com.fedeflores.superheroes.model.Superhero;
+
+import java.util.List;
+
 public interface SuperheroesService {
+
+    List<Superhero> getAllSuperheroes();
+
+    Superhero getSuperheroById(int id);
+
+    List<Superhero> getSuperheroesByName(String name);
+
+    Superhero updateSuperhero(int id, Superhero superhero);
+
+    void deleteSuperhero(int id);
+
+
 }
